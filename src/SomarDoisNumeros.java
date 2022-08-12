d514351612eb17cbabc1fa6e3a00120c94c6ec24
Pedro Gabriel <pedrogabrielbhz@gmail.com>
2022-08-12 15:57:46 -0300

    Add initial source code

---
+++ src/SomarDoisNumeros.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class SomarDoisNumeros {
+
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		int num1, num2, soma;
+		
+		System.out.println("Digite um número qualquer:");
+		num1 = sc.nextInt();
+
+		System.out.println("Digite outro número qualquer:");
+		num2 = sc.nextInt();
+		
+		soma = num1 + num2;
+		
+		System.out.println("Soma:" + soma);
+		
+	}
+
+}
